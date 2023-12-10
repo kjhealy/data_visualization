b4d0bb44426f99b2b4c1fb8ade90be808c1db556
Kieran Healy <kjhealy@gmail.com>
2023-12-10 09:59:57 -0500

    deploy sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+## Data Visualization deploy
+## Requires if (Sys.info()["user"] == "kjhealy") to be TRUE in _targets.R
+PUBLIC_DIR='_site/'
+SSH_USER='kjhealy@kjhealy.co'
+DOCUMENT_ROOT='~/public/vsd/public_html'
+
+echo "Uploading changes to remote server..."
+cp html/htaccess $PUBLIC_DIR/.htaccess
+rsync --exclude='.DS_Store' -Prvzce 'ssh -p 22' $PUBLIC_DIR $SSH_USER:$DOCUMENT_ROOT --delete-after
